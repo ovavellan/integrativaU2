7528f70423ae0983a1d15d2fda640e07aa05a2b3
ovavellan <oscaravellan2000@gmail.com>
2023-01-21 16:45:07 -0500

    tercer commi

--- Proyecto_Restaurante/src/app/app-routing.module.ts
+++ Proyecto_Restaurante/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'productos',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
--- Proyecto_Restaurante/src/app/productos/productos.service.ts
+++ Proyecto_Restaurante/src/app/productos/productos.service.ts
@@ -18,6 +18,62 @@ export class ProductosService {
       titulo:'Pizza',
       url: "https://www.saborusa.com/do/wp-content/uploads/sites/8/2019/11/Animate-a-disfrutar-una-deliciosa-pizza-de-salchicha-Foto-destacada.png",
       comentarios: ["Deliciosa Pizza de 4 estaciones"]
+    },
+
+    {
+      id: '3',
+      titulo:'Hot Dog',
+      url: "https://www.maxionline.ec/wp-content/uploads/2021/05/hot-dog-chile-menu.jpg",
+      comentarios: ["Deliciosa Hot Dog Americano"]
+    },
+
+    {
+      id: '4',
+      titulo:'Alitas Picantes',
+      url: "https://images.hola.com/imagenes/cocina/recetas/20200205159814/alitas-de-pollo-a-la-americana-buffalo-wings/0-778-671/aitas-de-pollo-a-la-americana-o-buffalo-wings-t.jpg",
+      comentarios: ["Deliciosa Alitas Picantes"]
+    },
+
+    {
+      id: '5',
+      titulo:'Papas Fritas',
+      url: "https://scm-assets.constant.co/scm/unilever/2bb5223be0548fcc55c230aa5f951219/84b57886-fee2-4f98-8920-027f8afd654c.png",
+      comentarios: ["Deliciosas Papas fritas"]
+    },
+
+    {
+      id: '6',
+      titulo:'Pizza de champiñones',
+      url: "https://static.guiainfantil.com/pictures/recetas/4797-4-pizza-vegetal-de-champinones-y-esparragos-trigueros.jpg",
+      comentarios: ["Deliciosas pizza de champiñones y pepperoni"]
+    },
+
+    {
+      id: '7',
+      titulo:'Western Bacon Doble',
+      url: "https://file.adomicil.io/carlsjr.adomicil.io/_files/images/product/burgers04doublewesternbaconchesseburgercombo-0641235177703777.jpg",
+      comentarios: ["Deliciosas Papas fritas"]
+    },
+
+    {
+      id: '8',
+      titulo:'Shawarma',
+      url: "https://www.comedera.com/wp-content/uploads/2022/03/Shawarma-de-cordero-shutterstock_1662680437.jpg",
+      comentarios: ["Delicioso Shawarma"]
+    },
+
+    {
+      id: '9',
+      titulo:'Tacos',
+      url: "https://www.pequerecetas.com/wp-content/uploads/2020/10/tacos-mexicanos.jpg",
+      comentarios: ["Deliciosos tacos Mexicanos"]
+    },
+
+    {
+      id: '10',
+      titulo:'Churros',
+      url: "https://www.hogarmania.com/archivos/202110/como-hacer-churros-caseros-sin-churrera-receta-1280x720x80xX.jpg",
+      comentarios: ["Deliciosos Churros"]
     }
   ];
 
